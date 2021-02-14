17bc814e8718ea830e003fafcb18dec33d1f9d64
AgentKnipe <aknipe@comcast.net>
2021-02-14 11:12:19 -0500

    add basic player movement
    add basic player movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,8 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour{
+    private float _facingDirection = 1f;
+
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private Rigidbody2D _rigidBody;
+
     // Start is called before the first frame update
     void Start(){
         
@@ -10,6 +19,38 @@ public class CharacterController : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        
+    }
+
+    void FixedUpdate() {
+        var input = Input.GetAxis("Horizontal");
+        _rigidBody.velocity = new Vector2(input * _speed, 0);
+
+        Debug.Log(input);
+
+        CheckIfShouldFlip(input);
+    }
+
+
+    private void CheckIfShouldFlip(float xInput) {
+        var direction = 0.0f;
+
+        if(xInput > 0) {
+            direction = 1f;
+        }
+        else if(xInput < 0) {
+            direction = -1f;
+        }
+        else {
+            direction = 0f;
+        }
+
+        if (direction != 0 && direction != _facingDirection) {
+            Flip();
+        }
+    }
+
+    private void Flip() {
+        _facingDirection *= -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
