93d895db0e4a0117a01c9136e2d680c3d07f0c26
prchristie <patrick.christie.dev@gmail.com>
2021-02-15 11:02:19 +1030

    Fixed a bug with the character controller that would reset the y velocity instead of allowing gravity to have an additive effect and added logic to switch between animations

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour{
-    private float _facingDirection = 1f;
+    private bool _facingRight = false;
 
     [SerializeField]
     private float _speed = 5f;
 
-    [SerializeField]
-    private Rigidbody2D _rigidBody;
+    private Rigidbody2D _rigidbody;
+
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start(){
-        
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,35 +24,47 @@ public class CharacterController : MonoBehaviour{
     }
 
     void FixedUpdate() {
-        var input = Input.GetAxisRaw("Horizontal");
-        _rigidBody.velocity = new Vector2(input * _speed, 0);
-
-        Debug.Log(input);
-
-        CheckIfShouldFlip(input);
+        Move();
     }
 
+    private void Move() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Handles the walking/running animation. Should likely be moved to a function but will
+        // worry about it when/if there are more animations.
+        if(horizontalInput != 0) {
+            _animator.SetBool("walking", true);
+        } else {
+            _animator.SetBool("walking", false);
+        }
+
+        // Set the velocity to the frame normalized time * the input on the x and maintain the y
+        // velocity so that gravity works correctly.
+        Vector2 velocity = new Vector2(
+            horizontalInput * _speed * Time.deltaTime,
+            _rigidbody.velocity.y
+        );
+
+        // Update the velocity with the player input velocity
+        _rigidbody.velocity = velocity;
+
+        CheckIfShouldFlip(velocity.x);
+    }
 
     private void CheckIfShouldFlip(float xInput) {
-        var direction = 0.0f;
-
-        if(xInput > 0) {
-            direction = -1f;
-        }
-        else if(xInput < 0) {
-            direction = 1f;
-        }
-        else {
-            direction = 0f;
-        }
-
-        if (direction != 0 && direction != _facingDirection) {
+        if(xInput > 0 && !_facingRight) {
+            Flip();
+        } else if(xInput < 0 && _facingRight) {
             Flip();
         }
     }
 
     private void Flip() {
-        _facingDirection *= -1;
-        transform.Rotate(0.0f, 180.0f, 0.0f);
+        _facingRight = !_facingRight;
+
+        // Multiply the player's x local scale by -1.
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
     }
 }
