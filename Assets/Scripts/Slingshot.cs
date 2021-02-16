fad539376a21937059a142c1d4108388db53b9d1
prchristie <patrick.christie.dev@gmail.com>
2021-02-16 12:45:40 +1030

    Added an example mechanic implementation

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Slingshot : Mechanic
+{
+    public override void onStartCallback()
+    {
+        mechanicAnimator.SetBool("is_slingshotting", true);
+        StartCoroutine(StopAnimation());
+    }
+
+    IEnumerator StopAnimation() {
+        yield return new WaitForSeconds(10);
+        mechanicAnimator.SetBool("is_slingshotting", false);
+        Finish();
+    }
+}
