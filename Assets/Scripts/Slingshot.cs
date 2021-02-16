d9fc1a7bcd0b1b58b90f265fa2a3c18c4dc6bda6
prchristie <patrick.christie.dev@gmail.com>
2021-02-17 00:22:46 +1030

    Realized I need the variable to tell it to stop allowing shooting

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -10,6 +10,8 @@ public class Slingshot : Mechanic
 
     [SerializeField]
     float _slingForceMultiplier = 2f;
+
+    private bool _controlling = false;
     private Rigidbody2D _rb;
 
     protected override void Start() {
@@ -18,9 +20,13 @@ public class Slingshot : Mechanic
     }
 
     private void Update() {
+        if(!_controlling)
+            return;
+
         if(Input.GetButtonDown("Fire1")) {
             FireSlingshot();
 
+            _controlling = false;
             _mechanicAnimator.SetBool("is_slingshotting", false);
             StartCoroutine(StopMechanic());
         }
@@ -44,13 +50,15 @@ public class Slingshot : Mechanic
 
     protected override void onStartCallback()
     {
+        _controlling = true;
         _mechanicAnimator.SetBool("is_slingshotting", true);
     }
 
     private IEnumerator StopMechanic() {
         yield return new WaitForSeconds(1.5f);
 
-        Finish();
+        if(!_controlling)
+            Finish();
     }
 
     private void OnDrawGizmos() {
