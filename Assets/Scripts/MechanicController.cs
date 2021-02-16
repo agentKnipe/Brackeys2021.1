dbbea5f6080ee9276ccc0c1f3549645f136cf217
prchristie <patrick.christie.dev@gmail.com>
2021-02-16 13:11:20 +1030

    Some additional comments explaining some things

--- Assets/Scripts/MechanicController.cs
+++ Assets/Scripts/MechanicController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class MechanicController : MonoBehaviour
 {
-    [Tooltip("Which key triggers the corresponding mechanic")]
+    [Tooltip("Which key triggers the corresponding mechanic.")]
     [SerializeField]
     List<KeyCode> keys;
+
+    [Tooltip("The mechanic that will be used when the key is pressed.")]
     [SerializeField]
     List<Mechanic> mechanics;
 
+    [Tooltip("Is played when any of the provided mechanic keys are pressed")]
     [SerializeField]
     ParticleSystem poofEffect;
 
@@ -25,7 +28,7 @@ public class MechanicController : MonoBehaviour
         // Just ensures that the lists are of equal length. This is because Unity cannot serialize
         // dictionaries :/
         if(keys.Count != mechanics.Count) {
-            Debug.Log("keys, animations and callbacks length not equal");
+            Debug.Log("keys and mechanics length not equal");
         }
     }
 
@@ -51,6 +54,9 @@ public class MechanicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks if any of the keys are pressed, and if they are then starts the mechanic.
+        // Likely needs a way to stop additional presses so that mechanics cant be used while in a
+        // mechanic.
         for(int i = 0; i < keys.Count; i++) {
             if(Input.GetKeyDown(keys[i])) {
                 Mechanic m = mechanics[i];
@@ -59,6 +65,7 @@ public class MechanicController : MonoBehaviour
         }
     }
 
+    // A coroutine so that there can be a delay between the poof effect and the mechanic starting.
     IEnumerator StartMechanic(Mechanic m) {
         ParticleSystem effect = Instantiate(poofEffect, transform.position, transform.rotation);
         effect.gameObject.transform.localScale *= m.size;
