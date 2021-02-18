09a3654d8ff3fa7fbd34a442fd8855a76f8dcd20
AgentKnipe <aknipe@comcast.net>
2021-02-18 13:11:13 -0500

    Add Level Cleared Menu
    Add a level cleared menu that gives the option continue to the next level (if one exists).

---
+++ Assets/Scripts/Objective/ObjectectiveController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectectiveController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //The player hit the objective, trigger win screen
+        if(collision.collider.name == "Player") {
+            LevelManager.LevelManagerInstance.LevelCleared();
+        }
+    }
+}
